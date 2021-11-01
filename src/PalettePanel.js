27f55ee37ab2172a72cf2f842d892080777892c2
李彦凯 <yancy@hightopo.com>
2021-11-01 14:36:44 +0800

    更新交互

--- src/PalettePanel.js
+++ src/PalettePanel.js
@@ -2,6 +2,15 @@ Palette.PalettePanel = function () {
     var self = this;
     Palette.PalettePanel.superClass.constructor.call(this);
 
+    // 点击 header 时即切换展开合并状态
+    self.getView().addEventListener(Default.isTouchable ? 'touchstart': 'mousedown', function (e) {
+        if (self.isHeaderVisible()) {
+            var lp = self.lp(e);
+            if (lp.y < self.getHeaderHeight()) {
+                self.setExpanded(!self.isExpanded());
+            }
+        }
+    });
 
     // 展开合并状态变化时同步到 JSON
     self.on('p:expanded', function(e) {
@@ -21,6 +30,9 @@ def('ht.ui.Palette.PalettePanel', ui.Panel, {
     __border: null,
     __borderRadius: 0,
 
+    getInteractorClasses: function() {
+        return [];
+    },
     getSerializableProperties: function () {
         var parentProperties = Palette.PalettePanel.superClass.getSerializableProperties.call(this);
         delete parentProperties.children;
